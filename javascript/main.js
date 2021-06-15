aa76fccdb30d4ae08055315fc65f7a6052827a0f
Amit <amitbd590@gmail.com>
2021-06-16 01:22:49 +0600

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,18 @@
+
+  
+// var typed = new Typed('.type', {
+//     strings: ['real values!', 'popular profit!'],
+//   smartBackspace: false, // Default value
+//   loop: true,
+//   typeSpeed: 150
+    
+//   });
+  
+
+  var typed = new Typed('.type', {
+    // Waits 1000ms after typing "First"
+    strings: ['real value', 'real profit'],
+    loop: true,
+  typeSpeed: 150
+  });
+  
