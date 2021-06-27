7e4157fa8370f739aed54facab574d98bf7daf12
Amit <amitbd590@gmail.com>
2021-06-27 18:18:22 +0600

    contact page add

--- javascript/main.js
+++ javascript/main.js
@@ -13,9 +13,17 @@ var typed = new Typed('.type', {
 
 $("#plus").click(function () {
   $("#effect").addClass("plus")
-})
+});
 
 
+
+function myMap() {
+  var mapProp = {
+      center:new google.maps.LatLng(51.508742,-0.120850),
+      zoom:5,
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  }
 
 
 
